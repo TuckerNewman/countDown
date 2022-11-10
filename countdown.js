06b27985654db082a25b15fc5a2d691aa6f4c1c4
Tucker Newman <82122015+TuckerNewman@users.noreply.github.com>
2022-11-10 08:39:13 -0500

    Initial upload
    Initial upload of code

---
+++ countdown.js
@@ -0,0 +1,13 @@
+const oneDay = 1000 * 60 * 60 * 24; //milliseconds in a day
+
+let today = new Date(); //get today
+let moveIn = new Date("2022-12-2");
+
+let timeDifference = moveIn.getTime() - today.getTime(); //find difference in time
+
+let dayDifference = Math.round(timeDifference/oneDay); //find difference in days
+
+console.log(dayDifference);
+
+
+document.getElementById('days-left').innerHTML = dayDifference;
