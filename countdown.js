b329feb99b66e3626418da307f0847eaab1b78fb
Tucker Newman <82122015+TuckerNewman@users.noreply.github.com>
2022-11-10 08:45:36 -0500

    Modified moveIn
    Added T character and time to accommodate mobile phone IOS

--- countdown.js
+++ countdown.js
@@ -1,7 +1,7 @@
 const oneDay = 1000 * 60 * 60 * 24; //milliseconds in a day
 
 let today = new Date(); //get today
-let moveIn = new Date("2022-12-2");
+let moveIn = new Date("2022-12-02T00:00:00");
 
 let timeDifference = moveIn.getTime() - today.getTime(); //find difference in time
 
